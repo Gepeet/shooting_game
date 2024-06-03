26a68fc122fab73d82f2010996a8ab829ba37e7d
Gepeet <letskeddotcom@gmail.com>
2024-06-03 08:25:01 +0800

    adding animation and parallax bg

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flame/components.dart';
 import 'package:flame/input.dart';
+import 'package:flame/parallax.dart';
 
 void main(){
    runApp(GameWidget(game:SpaceShootingGame()));
@@ -13,6 +14,18 @@ class SpaceShootingGame extends FlameGame with PanDetector{
 
    @override
    Future<void>onLoad() async {
+      final parallax = await loadParallaxComponent(
+         [
+            ParallaxImageData('stars_0.png'),
+            ParallaxImageData('stars_1.png'),
+            ParallaxImageData('stars_2.png'),
+         ],
+         baseVelocity: Vector2(0, -5),
+         repeat: ImageRepeat.repeat,
+         velocityMultiplierDelta: Vector2(0,5),
+   );
+      add(parallax);
+
       player = Player();
       add(player);
    }
@@ -23,7 +36,7 @@ class SpaceShootingGame extends FlameGame with PanDetector{
    }
 }
 
-class Player extends SpriteComponent with HasGameReference<SpaceShootingGame>{
+class Player extends SpriteAnimationComponent with HasGameReference<SpaceShootingGame>{
 
    Player()
       :super(
@@ -35,10 +48,16 @@ class Player extends SpriteComponent with HasGameReference<SpaceShootingGame>{
    Future<void> onLoad() async{
       await super.onLoad();
 
-      sprite = await game.loadSprite('player-sprite.png');
+      animation = await game.loadSpriteAnimation(
+         'player.png',
+         SpriteAnimationData.sequenced(
+            amount: 4,
+            stepTime: .2,
+            textureSize: Vector2(32,48),
+         )
+      );
 
       position = game.size/2;
-      anchor = Anchor.center;
    }
 
 
